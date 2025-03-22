be3a706b45d1be3dce7f623a13dcad33fa16f4f5
AJOY SAHA <ajoy.code.dreamer@gmail.com>
2025-03-22 22:12:49 +0600

    create 3 player game but not complete

--- js/script.js
+++ js/script.js
@@ -10,63 +10,80 @@ let error_text = document.querySelector(".error_text");
 let chance = document.querySelector(".chance");
 let chance_span = document.querySelector("span");
 let chance_count = 5;
-chance_span = chance_count
-
-
+let player2Result;
 
 player_1_btn.addEventListener("click", function () {
-    if(isNaN(player_1_inp.value) || player_1_inp.value == ""){    
-        player_1_inp.value = ""
-        error_text.innerHTML = "Please enter a number";
+  if (isNaN(player_1_inp.value) || player_1_inp.value == "") {
+    player_1_inp.value = "";
+    error_text.innerHTML = "Please enter a number";
+  } else {
+    if (player_1_inp.value > 10 || player_1_inp.value < 1) {
+      error_text.innerHTML = "Please enter a number between 1 and 10";
+    } else {
+      error_text.innerHTML = "";
+      player_name.innerHTML = "Player-2";
+      player_1_inp.style.display = "none";
+      player_1_btn.style.display = "none";
+      player_2_inp.style.display = "inline-block";
+      player_2_btn.style.display = "inline-block";
+      chance.style.display = "block";
+      chance_span.innerHTML = chance_count;
     }
-    else{
-        if(player_1_inp.value > 10 || player_1_inp.value < 1){
-            error_text.innerHTML = "Please enter a number between 1 and 10";
-        }
-        else{
-            error_text.innerHTML = "";
-            player_name.innerHTML = "Player-2";
-            player_1_inp.style.display = "none";
-            player_1_btn.style.display = "none";
-            player_2_inp.style.display = "inline-block";
-            player_2_btn.style.display = "inline-block";
-            console.log(player_1_inp.value);
-        }
-    }
-
-})
-
+  }
+});
 
 player_2_btn.addEventListener("click", function () {
-    if(isNaN(player_2_inp.value) || player_2_inp.value == ""){    
-        error_text.innerHTML = "Please enter a number";
-    }
-    else{
-        if(player_2_inp.value > 10 || player_2_inp.value < 1){
-            error_text.innerHTML = "Please enter a number between 1 and 10";
+  if (isNaN(player_2_inp.value) || player_2_inp.value == "") {
+    error_text.innerHTML = "Please enter a number";
+  } else {
+    if (player_2_inp.value > 10 || player_2_inp.value < 1) {
+      error_text.innerHTML = "Please enter a number between 1 and 10";
+    } else {
+      if (chance_count > 0) {
+        chance_count--;
+        chance_span.innerHTML = chance_count;
+        if (player_2_inp.value == player_1_inp.value) {
+          player2Result = "win";
+          error_text.innerHTML = "";
+          player_name.innerHTML = "Player-3";
+          player_1_inp.style.display = "none";
+          player_1_btn.style.display = "none";
+          player_2_inp.style.display = "none";
+          player_2_btn.style.display = "none";
+          player_3_inp.style.display = "inline-block";
+          player_3_btn.style.display = "inline-block";
+          chance.style.display = "block";
+          chance_span.innerHTML = chance_count;
         }
-        else{
-            if(player_2_inp.value == player_1_inp.value){
-                error_text.innerHTML = "plyer 2 win"
-            }
-            else{
-                error_text.innerHTML = "player 1 win"
-            }
-        }
+      } else {
+        player2Result = "lose";
+      }
     }
+  }
+});
 
-})
-
-
-
-
-
-
-
-
-
-
-
-
+player_3_btn.addEventListener("click", function () {
+  if (isNaN(player_3_inp.value) || player_3_inp.value == "") {
+    error_text.innerHTML = "Please enter a number";
+  } else {
+    if (player_3_inp.value > 10 || player_3_inp.value < 1) {
+      error_text.innerHTML = "Please enter a number between 1 and 10";
+    } else {
+        chance_count = 5;
+      if (chance_count > 0) {
+        chance_count--;
+        chance_span.innerHTML = chance_count;
+        if (
+          player_3_inp.value == player_1_inp.value &&
+          player2Result == "win"
+        ) {
+          error_text.innerHTML = "Player-2 Player-3 wins";
+        }
+      } else {
+        player2Result = "lose";
+      }
+    }
+  }
+});
 
 // ========== guess game ==========
